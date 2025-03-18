bf01673b65f64182c847acdb0207d06088c6cd03
Elena Slavina <elena@dreams.quest>
2025-03-18 14:40:08 +0300

    Added a dialog to test whether the player has met the NPC before

--- Assets/Scripts/DialogueSystem/Enums/EDialogueCondition.cs
+++ Assets/Scripts/DialogueSystem/Enums/EDialogueCondition.cs
@@ -2,14 +2,14 @@
 {
     public enum EDialogueCondition
     {
-        IsGender,            // 0
-        HasMet,              // 1
-        IsReputationAmount,  // 2
-        IsRace,              // 3
-        IsQuestState,        // 4
-        HasEnoughItems,      // 5
-        HasEnoughCoins,      // 6
-        IsDialogueVariable,  // 7
-        IsFriendshipAmount   // 8
+        IsGender,
+        IsRace,
+        HasMet,
+        IsReputationAmount,
+        IsQuestState,
+        HasEnoughItems,
+        HasEnoughCoins,
+        IsDialogueVariable,
+        IsFriendshipAmount
     }
 }
